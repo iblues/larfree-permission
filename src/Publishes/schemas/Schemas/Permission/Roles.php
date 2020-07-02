5846b5c5f44d5a31ee99ccf419f4a4c62537d7ec
Blues <i@iblues.name>
2020-07-02 18:01:47 +0800

    配置

--- src/Publishes/schemas/Schemas/Permission/Roles.php
+++ src/Publishes/schemas/Schemas/Permission/Roles.php
@@ -26,7 +26,7 @@ return [
         ],
 
         'nav' => [
-            'name' => '菜单权限',
+            'name' => '菜单类型',
             'tip' => '',
             'type' => 'tree',
             //哪怕link没有值, 也需要存在,代表他有值
@@ -41,16 +41,15 @@ return [
         ],
 
         'api' => [
-            'name' => '接口权限',
-            'tip' => '正常情况可全选',
-            'type' => 'select',
-            //先暂停该字段. 会引起sync多重删除
-//            'link' =>[],
+            'name' => '接口类型',
+            'tip' => '普通情况可全选',
+            'type' => 'tree',
+            //先暂停该字段
+            'link' =>[],
             'component_param'=>[
-                'key'=>'target_id',
-                'value'=>'name',
-//                'api'=>'/{$COMPONENT_API}/nav/tree',
-                'api'=>'/admin/nav/tree'//指定api地址.
+                'key'=>'id',
+                'value'=>'comment',
+                'api'=>'/permission/permission/api/tree '//指定api地址.
             ],
             'sql_type' => 'varchar(191)',
         ],
